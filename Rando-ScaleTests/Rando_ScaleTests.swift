de980d0a14c11f1ac3e16cd9921cb1ad7ac4eb8a
The Clout <Theclout@imac>
2018-06-11 21:18:35 -0500

    Initial Commit

---
+++ Rando-ScaleTests/Rando_ScaleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rando_ScaleTests.swift
+//  Rando-ScaleTests
+//
+//  Created by The Clout on 6/11/18.
+//  Copyright © 2018 Draper Web Services. All rights reserved.
+//
+
+import XCTest
+@testable import Rando_Scale
+
+class Rando_ScaleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
