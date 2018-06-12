a142cced22990e72cb57f642635cc3ae38c57489
The Clout <Theclout@imac.attlocal.net>
2018-06-11 22:06:40 -0500

    Added Table

--- Rando-Scale/ViewController.swift
+++ Rando-Scale/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 4
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "scaleTypeCell")
+        
+        cell.textLabel?.text = "Top Row"
+        
+        return cell
+        
+    }
+    
+    @IBOutlet weak var scaleTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
