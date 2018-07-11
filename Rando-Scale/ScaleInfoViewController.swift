61f6dfe235cc7d416ba46e7bb1f3961974a7bec8
The Clout <Theclout@imac.attlocal.net>
2018-07-11 02:12:59 -0500

    Added 'scale info' view and navigation controllers

---
+++ Rando-Scale/ScaleInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScaleInfoViewController.swift
+//  Rando-Scale
+//
+//  Created by The Clout on 7/11/18.
+//  Copyright © 2018 Draper Web Services. All rights reserved.
+//
+
+import UIKit
+
+class ScaleInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
